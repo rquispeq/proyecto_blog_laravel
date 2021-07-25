f7910f9d50d138ffe08a43c67f4bd19fa86430f2
Rony Quispe <oghmand@gmail.com>
2021-07-25 18:23:31 -0500

    Cambia información del footer.

--- resources/views/layouts/template/admin/footer.blade.php
+++ resources/views/layouts/template/admin/footer.blade.php
@@ -1,8 +1,8 @@
 <!-- Main Footer -->
 <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    <strong>Copyright &copy; 2021 Creado por <a href="https://github.com/rquispeq">Rony</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 3.1.0
+        <b>Version</b> 1.0
     </div>
 </footer>
