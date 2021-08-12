f85a48f25a9833efe3382b82eafed16124b2cea4
Rony Quispe <oghmand@gmail.com>
2021-08-11 19:41:56 -0500

    Agrega listado de categorias dinamico.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Post;
 use App\Models\Tag;
 use Illuminate\Http\Request;
@@ -25,8 +26,9 @@ class HomeController extends Controller
     public function index()
     {
         $posts = Post::where('active',1)->orderBy('created_at','desc')->get();
-        
         $tags = Tag::where('active',1)->get();
-        return view('home',compact('posts','tags'));
+        $categories = Category::where('active',1)->get();
+        
+        return view('home',compact('posts','tags','categories'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -111,12 +111,9 @@
                                     </div>
                                     <div class="content">
                                         <ul>
-                                            <li><a href="#">- Nature Lifestyle</a></li>
-                                            <li><a href="#">- Awesome Layouts</a></li>
-                                            <li><a href="#">- Creative Ideas</a></li>
-                                            <li><a href="#">- Responsive Templates</a></li>
-                                            <li><a href="#">- HTML5 / CSS3 Templates</a></li>
-                                            <li><a href="#">- Creative &amp; Unique</a></li>
+                                            @foreach($categories as $category)
+                                            <li><a href="#">- {{$category->name}}</a></li>
+                                            @endforeach
                                         </ul>
                                     </div>
                                 </div>
